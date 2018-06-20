ee15408f17ae04aa074793fa8b1c4881a4ac62de
Mo Bitar <me@bitar.io>
2018-06-19 19:15:22 -0500

    Fixes search hopping issue, forum #234

--- app/javascripts/main/searchManager.js
+++ app/javascripts/main/searchManager.js
@@ -4,9 +4,8 @@ class SearchManager {
 
   constructor() {
     ipcMain.on('search-text', (event, text) => {
-      if(!text || text.length == 0) {
-        this.window.webContents.stopFindInPage('clearSelection');
-      } else {
+      this.window.webContents.stopFindInPage('clearSelection');
+      if(text && text.length > 0) {
         // This option arrangement is required to avoid an issue where clicking on a
         // different note causes scroll to jump.
         this.window.webContents.findInPage(text, {forward: true, findNext: false});
