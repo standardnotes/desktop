cd5cf46ce1350e68266dee8c32b5b094d3997fd4
Baptiste Grob <60621355+baptiste-grob@users.noreply.github.com>
2021-02-16 19:44:02 +0100

    chore(ci): show each process' stdout

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,8 +1,21 @@
-import { execSync } from 'child_process';
+import { spawn } from 'child_process';
 
-function runCommand(command) {
-  console.log(command);
-  execSync(command);
+function runCommand(fullCommand) {
+  return new Promise((resolve, reject) => {
+    console.log(fullCommand);
+    const [command, ...args] = fullCommand.split(' ');
+    const child = spawn(command, args);
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+    child.on('error', reject);
+    child.on('close', (code) => {
+      if (code > 0) {
+        reject(code);
+      } else {
+        resolve(code);
+      }
+    });
+  });
 }
 
 const appimage = 'appimage';
@@ -11,45 +24,53 @@ const snap = 'snap';
 const windows = 'windows';
 const availableTargets = [appimage, mac, snap, windows];
 
-try {
-  const target = process.argv[2];
-  if (!target) {
-    throw Error(
-      `No target specified. Available targets: ${availableTargets.join(', ')}`
-    );
-  } else if (!availableTargets.includes(target)) {
-    throw Error(
-      `Unknown target '${target}'. Available target: ${availableTargets.join(
-        ', '
-      )}`
-    );
-  }
+(async () => {
+  try {
+    const target = process.argv[2];
+    if (!target) {
+      throw Error(
+        `No target specified. Available targets: ${availableTargets.join(', ')}`
+      );
+    } else if (!availableTargets.includes(target)) {
+      throw Error(
+        `Unknown target '${target}'. Available target: ${availableTargets.join(
+          ', '
+        )}`
+      );
+    }
 
-  runCommand('yarn clean:build');
-  switch (target) {
-    case appimage:
-      runCommand('yarn run webpack --config webpack.prod.js');
-      runCommand(
-        'yarn run electron-builder --linux --x64 --ia32 -c.linux.target=AppImage'
-      );
-      break;
-    case mac:
-      runCommand('yarn run webpack --config webpack.prod.js --env snap');
-      runCommand('yarn run electron-builder --mac --x64');
-      runCommand('node scripts/fix-mac-zip');
-      break;
-    case snap:
-      runCommand('yarn run webpack --config webpack.prod.js --env snap');
-      runCommand(
-        'yarn run electron-builder --linux --x64 --ia32 -c.linux.target=snap'
-      );
-      break;
-    case windows:
-      runCommand('yarn run webpack --config webpack.prod.js --env snap');
-      runCommand('yarn run electron-builder --windows --x64 --ia32');
-      break;
+    await runCommand('yarn clean:build');
+    switch (target) {
+      case appimage:
+        await runCommand('yarn run webpack --config webpack.prod.js');
+        await runCommand(
+          'yarn run electron-builder --linux --x64 --ia32 -c.linux.target=AppImage'
+        );
+        break;
+      case mac:
+        await runCommand(
+          'yarn run webpack --config webpack.prod.js --env snap'
+        );
+        await runCommand('yarn run electron-builder --mac --x64');
+        await runCommand('node scripts/fix-mac-zip');
+        break;
+      case snap:
+        await runCommand(
+          'yarn run webpack --config webpack.prod.js --env snap'
+        );
+        await runCommand(
+          'yarn run electron-builder --linux --x64 --ia32 -c.linux.target=snap'
+        );
+        break;
+      case windows:
+        await runCommand(
+          'yarn run webpack --config webpack.prod.js --env snap'
+        );
+        await runCommand('yarn run electron-builder --windows --x64 --ia32');
+        break;
+    }
+  } catch (e) {
+    console.error(e);
+    process.exitCode = 1;
   }
-} catch (e) {
-  console.error(e.toString());
-  process.exitCode = 1;
-}
+})();
