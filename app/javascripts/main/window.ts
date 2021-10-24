ab0cdf3e1beb0e5d50e8f766b9472c04e1e22213
Mo Bitar <me@bitar.io>
2021-10-24 10:00:10 -0500

    deps: electron-clear-data 1.1.1

--- app/javascripts/main/window.ts
+++ app/javascripts/main/window.ts
@@ -18,7 +18,7 @@ import { checkForUpdate, setupUpdates } from './updateManager';
 import { isTesting, lowercaseDriveLetter } from './utils';
 import { initializeZoomManager } from './zoomManager';
 import { Paths } from './paths';
-import { clearSensitiveDirectories } from 'electron-clear-data';
+import { clearSensitiveDirectories } from '@standardnotes/electron-clear-data';
 
 const WINDOW_DEFAULT_WIDTH = 1100;
 const WINDOW_DEFAULT_HEIGHT = 800;
