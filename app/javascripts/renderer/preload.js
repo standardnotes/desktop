50a92bd96d995c1677d091c897185f750243491f
Johnny A <5891646+johnny243@users.noreply.github.com>
2022-03-24 09:42:38 -0700

    feat: integrate SecureSpellChecker (#848)
    * feat: integrate SecureSpellChecker (#830)
    
    * chore(deps): install @standardnotes/electron-secure-spellchecker
    
    * feat: integrate SecureSpellChecker
    
    * fix: formatting
    
    * chore: update app version
    
    * fix: use system spellchecker suggestions on macOS
    
    * chore(deps): bump electron-secure-spellchecker
    
    * chore(deps): update secure-spellchecker

--- app/javascripts/renderer/preload.js
+++ app/javascripts/renderer/preload.js
@@ -1,5 +1,6 @@
 import { IpcMessages } from '../shared/ipcMessages';
 import { Store, StoreKeys } from '../main/store';
+import SecureSpellChecker from '@standardnotes/electron-secure-spellchecker';
 const { Transmitter, FrameMessageBus, Validation } = require('sn-electron-valence/Transmitter');
 const { ipcRenderer } = require('electron');
 const path = require('path');
@@ -39,6 +40,7 @@ const transmitter = new Transmitter(messageBus, {
 process.once('loaded', function () {
   loadTransmitter();
   listenForIpcEvents();
+  SecureSpellChecker.setSpellCheckProvider();
 });
 
 function loadTransmitter() {
